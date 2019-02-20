1daccc04ac4bc68469e75edaf8fcbfa506dc182b
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-02-20 18:50:28 +0100

    Updates

--- precache-manifest.8d76e6ed658acce95513e819bb956fc5.js
+++ precache-manifest.8d76e6ed658acce95513e819bb956fc5.js
@@ -16,23 +16,23 @@ self.__precacheManifest = [
     "url": "/aeons-end-randomizer/static/js/2.5aeda535.chunk.js"
   },
   {
-    "revision": "bc54396771c64863193f7a3fe93d9cff",
-    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.bc543967.ttf"
+    "revision": "e31a701aa9b88125d36119acb8a10482",
+    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.e31a701a.woff"
   },
   {
     "revision": "cbfd0298f008558557418d19abfc2f42",
     "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.cbfd0298.eot"
   },
   {
-    "revision": "e31a701aa9b88125d36119acb8a10482",
-    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.e31a701a.woff"
+    "revision": "bc54396771c64863193f7a3fe93d9cff",
+    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.bc543967.ttf"
   },
   {
     "revision": "c243adb601cb0e965344851b1c46194f",
     "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.c243adb6.svg"
   },
   {
-    "revision": "9b57cc6cbec8415201a60d0d46a35f7a",
+    "revision": "b86aaa54fe2d72da9b9869c1fa1b3789",
     "url": "/aeons-end-randomizer/index.html"
   }
 ];
