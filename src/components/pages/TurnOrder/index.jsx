d874d2e373d7fc70be5ede7676a61c8a3a20a1f8
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-02-24 08:20:45 +0100

    [FEATURE] Persist turn order game state + refactoring
    Closes: #34, #32
    
    * Also reverses turn order discard

--- src/components/pages/TurnOrder/index.jsx
+++ src/components/pages/TurnOrder/index.jsx
@@ -1,107 +1,27 @@
-import React, { useState } from 'react'
-
-import config from 'config'
-
-import {
-  createEntityList
-} from '../helpers'
+import React from 'react'
 
 import ActiveTurnOrder from './ActiveTurnOrder'
 import TurnOrderSetup from './TurnOrderSetup'
+import {
+  useTurnOrderSetup,
+  useGameState
+} from './hooks'
 
-
-const shuffleDeck = (deck) => {
-  return createEntityList(deck, deck)
-}
 
 const TurnOrder = () => {
-  // Turn order setup handling
-  const [turnOrderSetup, setTurnOrderSetup] = useState(config.TURNORDERSETUPS['onePlayerThreeToc'])
-  const handleTurnOrderSetup = (event) => {
-    setTurnOrderSetup(config.TURNORDERSETUPS[event.target.value])
-  }
+  const { turnOrderSetup, handleTurnOrderSetupChange } = useTurnOrderSetup()
 
-  // Game state handling
-  const [gameState, setGameState] = useState({
-    started: false
-  })
-
-  const handleStartGame = () => {
-    setGameState({
-      started: true
-    })
-
-    setTurnOrderState({
-      ...turnOrderState,
-      deck: shuffleDeck(turnOrderSetup.turnOrderCards)
-    })
-  }
-
-  const handleResetGame = () => {
-    setGameState({
-      started: false
-    })
-
-    setTurnOrderState({
-      ...turnOrderState,
-      discard: []
-    })
-  }
-
-  // Turn order state handling
-  const [turnOrderState, setTurnOrderState] = useState({
-    deck: [],
-    discard: []
-  })
-
-  const drawTurnOrderCard = () => {
-    const { deck } = turnOrderState
-
-    if(deck.length === 0) return
-
-    const cardDrawn = deck[deck.length - 1];
-
-    setTurnOrderState({
-        deck: deck.filter(card => card.id !== cardDrawn.id),
-        discard: [...turnOrderState.discard, cardDrawn]
-    })
-  }
-
-  const startNewRound = () => {
-    setTurnOrderState({
-        deck: shuffleDeck(turnOrderSetup.turnOrderCards),
-        discard: []
-    })
-  }
-
-  const addToTop = (cardId) => {
-    const cardToAdd = turnOrderState.discard.find(card => card.id === cardId)
-
-    setTurnOrderState({
-        deck: [...turnOrderState.deck, cardToAdd],
-        discard: turnOrderState.discard.filter(card => card.id !== cardId)
-    })
-  }
-
-  const addToBottom = (cardId) => {
-    const cardToAdd = turnOrderState.discard.find(card => card.id === cardId)
-
-    setTurnOrderState({
-        deck: [cardToAdd, ...turnOrderState.deck],
-        discard: turnOrderState.discard.filter(card => card.id !== cardId)
-    })
-  }
-
-  const shuffleIntoDeck = (cardId) => {
-    const cardToShuffle = turnOrderState.discard.find(card => card.id === cardId)
-    const newDeck = [...turnOrderState.deck, cardToShuffle]
-    const shuffledDeck = shuffleDeck(newDeck)
-
-    setTurnOrderState({
-        deck: shuffledDeck,
-        discard: turnOrderState.discard.filter(card => card.id !== cardId)
-    })
-  }
+  const {
+    addToBottom,
+    addToTop,
+    drawTurnOrderCard,
+    gameState,
+    handleResetGame,
+    handleStartGame,
+    shuffleIntoDeck,
+    startNewRound,
+    turnOrderState
+  } = useGameState(turnOrderSetup)
 
   return (
     <React.Fragment>
@@ -124,7 +44,7 @@ const TurnOrder = () => {
             <TurnOrderSetup
               turnOrderSetup={turnOrderSetup}
               startGame={handleStartGame}
-              chooseSetup={handleTurnOrderSetup}
+              chooseSetup={handleTurnOrderSetupChange}
             />
           )}
     </React.Fragment>
