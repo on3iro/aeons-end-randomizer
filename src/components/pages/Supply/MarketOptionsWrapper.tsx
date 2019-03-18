6be9b801e6f957697a8b839b8face363f05cc52c
Janett Hickel <janett.hickel@googlemail.com>
2019-03-18 21:37:11 +0100

    [FEATURE] Add market setup preview

---
+++ src/components/pages/Supply/MarketOptionsWrapper.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+
+const MarketOptionsWrapper = styled('div')`
+  margin: 0 -8px;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export default MarketOptionsWrapper
---
+++ src/components/pages/Supply/helpers.ts
@@ -0,0 +1,17 @@
+import { Operation } from '../../../types'
+
+export const getOperationString = (
+  operation: Operation,
+  values?: number[],
+  threshold?: number
+) => {
+  if (operation === "OR" && values) {
+    return values.join("/")
+  }
+
+  const thresholdValue = threshold
+    ? threshold
+    : ""
+
+  return `${operation} ${thresholdValue}`
+}
