271694be60df35ccfa819817f577d22030f11fac
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-03-18 19:29:29 +0100

    Revert "[HOTFIX] Completely remove UpdateScreen for now"
    This reverts commit c0c71aabd422eddf97b77449bcdaea25c21ea49d.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import 'rpg-awesome/css/rpg-awesome.min.css'
+
+import axios from 'axios'
 
 import CssBaseline from '@material-ui/core/CssBaseline'
 import blue from '@material-ui/core/colors/blue';
@@ -10,6 +12,7 @@ import { withStyles } from '@material-ui/core/styles'
 
 import { styles } from './appStyles'
 
+import UpdateScreen from './UpdateScreen'
 import MainApp from './MainApp'
 
 
@@ -23,13 +26,44 @@ const theme = createMuiTheme({
   },
 });
 
-const App = React.memo(({ classes }: { classes: any }) => (
-  <MuiThemeProvider theme={theme}>
-    <div className={classes.root}>
-      <CssBaseline />
-      <MainApp classes={classes} />
-    </div>
-  </MuiThemeProvider>
-))
+type UpdateInformation = {
+  updateAvailable: boolean,
+  newVersion: string
+}
+
+const App = React.memo(({ classes }: { classes: any }) => {
+  const [ updateInformation, setUpdateAvailable ] = useState<UpdateInformation>({
+    updateAvailable: false,
+    newVersion: ''
+  })
+
+  useEffect(() => {
+    axios.get('https://api.github.com/repos/on3iro/aeons-end-randomizer/releases')
+      .then(response => {
+        const newestRelease = response.data[0]
+        const localAppVersion = process.env.REACT_APP_VERSION
+        const newVersion = newestRelease['tag_name']
+        if (newVersion !== `v${localAppVersion}`) {
+          setUpdateAvailable({
+            updateAvailable: true,
+            newVersion
+          })
+        }
+      })
+  }, [])
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        {
+          updateInformation.updateAvailable
+            ? <UpdateScreen newVersion={updateInformation.newVersion} />
+            : <MainApp classes={classes} />
+        }
+      </div>
+    </MuiThemeProvider>
+  );
+})
 
 export default withStyles(styles, { withTheme: true })(App)
