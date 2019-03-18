9aceded169472822243cff2eba5cead8ba3b6cde
Janett Hickel <janett.hickel@googlemail.com>
2019-03-18 21:37:11 +0100

    [FEATURE] Add market setup preview

--- src/components/pages/Supply/MarketOptions.tsx
+++ src/components/pages/Supply/MarketOptions.tsx
@@ -4,22 +4,52 @@ import { connect } from 'react-redux'
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
+import Typography from '@material-ui/core/Typography';
+
+import MarketOptionsWrapper from './MarketOptionsWrapper';
+import PreviewTile from './PreviewTile';
+import PreviewTileList from './PreviewTileList';
+import PreviewTileListItem from './PreviewTileListItem';
 
 import { IMarketSetup, IMarketSetups } from '../../../types'
 import { RootState } from '../../../Redux/Store'
 import * as SupplySelection from '../../../Redux/Store/Supply/Selection'
 import * as SupplySetups from '../../../Redux/Store/Settings/SupplySetups'
 
+import { getOperationString } from './helpers'
 
-const renderMarketOptions = (activeMarketSetups: ReadonlyArray<IMarketSetup>) =>
+const renderMarketSetupItems = (tiles: any, classes: any) => tiles.map( (tile: any, index: number) => (
+  <PreviewTileListItem key={index} className={`${classes[tile.type.toLowerCase()]}`}>
+    { getOperationString(tile.operation, tile.values, tile.threshold) }
+  </PreviewTileListItem>
+))
+
+const renderMarketOptionsNew = (
+  activeMarketSetups: ReadonlyArray<IMarketSetup>,
+  allMarketSetups: IMarketSetups,
+  expansionHandler: (
+    event: React.ChangeEvent<any> | undefined,
+    expanded: boolean | string
+  ) => void,
+  marketSetupId: string,
+  selectSetup: (setup: Readonly<IMarketSetup>) => SupplySelection.Action,
+  classes: any
+) =>
   activeMarketSetups
     .map(setup => (
-      <FormControlLabel
-        key={setup.id}
-        value={setup.id}
-        control={<Radio />}
-        label={setup.name}
-      />
+      <PreviewTile key={setup.id}
+        onClick={(event: any) => {
+          expansionHandler(undefined, false)
+          selectSetup(allMarketSetups[event.currentTarget.dataset.value])
+        }}
+        data-value={setup.id}
+        className={marketSetupId === setup.id ? classes.cardSelected : ''}
+      >
+        <PreviewTileList>
+          { renderMarketSetupItems(setup.tiles, classes) }
+        </PreviewTileList>
+        <Typography variant="caption" className={`${classes.cardLabel} ${(marketSetupId === setup.id) ? classes.cardLabelSelected : ''}`}>{setup.name}</Typography>
+      </PreviewTile>
     ))
 
 const MarketOptions = React.memo(({
@@ -28,6 +58,7 @@ const MarketOptions = React.memo(({
   expansionHandler,
   marketSetupId,
   selectSetup,
+  classes,
 }: {
   activeMarketSetups: ReadonlyArray<IMarketSetup>,
   allMarketSetups: IMarketSetups,
@@ -36,19 +67,19 @@ const MarketOptions = React.memo(({
     expanded: boolean | string
   ) => void,
   marketSetupId: string,
-  selectSetup: (setup: Readonly<IMarketSetup>) => SupplySelection.Action
+  selectSetup: (setup: Readonly<IMarketSetup>) => SupplySelection.Action,
+  classes: any
 }) => (
-  <RadioGroup
-    aria-label="Market Setup Options"
-    name="marketOptions"
-    value={marketSetupId}
-    onChange={(event: React.ChangeEvent<any>) => {
-      expansionHandler(undefined, false)
-      selectSetup(allMarketSetups[event.currentTarget.value])
-    }}
-  >
-    { renderMarketOptions(activeMarketSetups) }
-  </RadioGroup>
+  <MarketOptionsWrapper>
+    { renderMarketOptionsNew(
+      activeMarketSetups,
+      allMarketSetups,
+      expansionHandler,
+      marketSetupId,
+      selectSetup,
+      classes
+    ) }
+  </MarketOptionsWrapper>
 ))
 
 const mapStateToProps = (state: RootState) => ({
---
+++ src/components/pages/Supply/PreviewTileListItem.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/macro'
+
+const PreviewTileListItem = styled('li')`
+  margin: 4px;
+  min-width: 36px;
+  width: calc(33.33333% - 8px);
+  height: 36px;
+  border: 1px solid rgba(0, 0, 0, .12);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  color: rgba(0, 0, 0, .54);
+  font-family: 'Roboto', sans-serif;
+  
+  @media all and (min-width: 502px) {
+    width: calc(11.11111% - 8px);
+  }
+`
+
+export default PreviewTileListItem
