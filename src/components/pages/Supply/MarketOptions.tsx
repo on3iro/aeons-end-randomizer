02cbad01fb22726804d9e490c46bafec44c56afb
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-03-24 08:49:34 +0100

    [TASK]: Add makeMapStateToProps factory

--- src/components/pages/Supply/MarketOptions.tsx
+++ src/components/pages/Supply/MarketOptions.tsx
@@ -8,22 +8,27 @@ import * as SupplySetups from '../../../Redux/Store/Settings/SupplySetups'
 import MarketOptionsWrapper from './MarketOptionsWrapper'
 import SupplyPreview from '../../SupplyPreview'
 
-const MarketOptions = React.memo(
-  ({
-    activeMarketSetups,
-    expansionHandler,
-    classes,
-  }: {
-    activeMarketSetups: ReadonlyArray<IMarketSetup>
+const mapStateToProps = (state: RootState) => ({
+  activeMarketSetups: SupplySetups.selectors.getActiveSetups(state),
+})
+
+const mapDispatchToProps = {}
+
+type Props = ReturnType<typeof mapStateToProps> &
+  typeof mapDispatchToProps & {
+    classes: any
     expansionHandler: (
       event: React.ChangeEvent<any> | undefined,
       expanded: boolean | string
     ) => void
-    classes: any
-  }) => (
+  }
+
+const MarketOptions = React.memo(
+  ({ activeMarketSetups, expansionHandler, classes }: Props) => (
     <MarketOptionsWrapper>
       {activeMarketSetups.map(setup => (
         <SupplyPreview
+          key={setup.id}
           setup={setup}
           clickHandler={() => expansionHandler(undefined, false)}
           classes={classes}
@@ -32,12 +37,6 @@ const MarketOptions = React.memo(
     </MarketOptionsWrapper>
   )
 )
-
-const mapStateToProps = (state: RootState) => ({
-  activeMarketSetups: SupplySetups.selectors.getActiveSetups(state),
-})
-
-const mapDispatchToProps = {}
 
 export default connect(
   mapStateToProps,
