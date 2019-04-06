9f44a743e22916c07a0491893c467eb201b645d5
Janett Hickel <janett.hickel@googlemail.com>
2019-04-06 16:44:51 +0200

    [REFACTOR] Refactor nemeses styles

---
+++ src/components/pages/Nemeses/EmptyNemesisHint.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro'
+import Typography from '@material-ui/core/Typography'
+
+const EmptyNemesisHint = styled(Typography)``
+
+EmptyNemesisHint.displayName = 'EmptyNemesisHint'
+
+export default EmptyNemesisHint
---
+++ src/components/pages/Nemeses/NemesisTile/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+import MuiCard from '@material-ui/core/Card'
+
+const Card = styled(MuiCard)`
+  position: 'relative';
+`
+
+Card.displayName = 'Card'
+
+export default Card
