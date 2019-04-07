cc3bd7f724ac725f05935de2d2ead1b64ae78b9f
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-04-07 18:09:24 +0200

    [BUGFIX] Fix broken styling because of JSS insertion order

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles'
+
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import configureStore from './Redux/configureStore'
@@ -8,9 +13,19 @@ import { initialState } from './Redux/Store'
 
 const store = configureStore(initialState)
 
+const generateClassName = createGenerateClassName()
+// @ts-ignore
+const jss = create({
+  ...jssPreset(),
+  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+  insertionPoint: document.getElementById('jss-insertion-point'),
+})
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <JssProvider jss={jss} generateClassName={generateClassName}>
+      <App />
+    </JssProvider>
   </Provider>,
   document.getElementById('root')
 )
