5848b065d7ab68e05b4f7cd19624e5057ec7d07d
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-04-13 10:05:59 +0200

    [REFACTOR] Turnorder styles

--- src/components/pages/TurnOrder/TurnOrderConfiguration.tsx
+++ src/components/pages/TurnOrder/TurnOrderConfiguration.tsx
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles'
 
 import { RootState, actions, selectors } from '../../../Redux/Store'
 
-import turnOrderStyles from './styles'
 import ShuffleButton from '../../ShuffleButton'
 import CardNameDisplay from './CardNameDisplay'
 import ModeSelection from './ModeSelection'
@@ -24,18 +23,10 @@ const mapDispatchToProps = {
   startGame: actions.TurnOrder.ActiveGame.startGame,
 }
 
-type Props = ReturnType<typeof mapStateToProps> &
-  typeof mapDispatchToProps & {
-    classes: any
-  }
+type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps & {}
 
 const TurnOrderConfiguration = React.memo(
-  ({
-    currentConfiguration,
-    startGame,
-    selectedPlayerCount,
-    classes,
-  }: Props) => {
+  ({ currentConfiguration, startGame, selectedPlayerCount }: Props) => {
     const availableVariations = Object.keys(selectedPlayerCount.variations).map(
       key => [selectedPlayerCount.variations[key]]
     )
@@ -45,12 +36,9 @@ const TurnOrderConfiguration = React.memo(
     return (
       <React.Fragment>
         <PlayerCountSelection />
-        {hasMoreThanOneVariant ? <SetupSelection classes={classes} /> : null}
-        <ModeSelection classes={classes} />
-        <CardNameDisplay
-          classes={classes}
-          turnOrderSetup={currentConfiguration}
-        />
+        {hasMoreThanOneVariant ? <SetupSelection /> : null}
+        <ModeSelection />
+        <CardNameDisplay turnOrderSetup={currentConfiguration} />
         <ShuffleButton
           color="primary"
           variant="extended"
@@ -68,4 +56,4 @@ TurnOrderConfiguration.displayName = 'TurnOrderConfiguration'
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withStyles(turnOrderStyles)(TurnOrderConfiguration))
+)(TurnOrderConfiguration)
