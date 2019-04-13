ae862a5d385f0f05f7c1aa5d1d83d90d4721ddc4
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-04-13 12:01:40 +0200

    [REFACTOR] Turnorder styling and mainTheme

---
+++ src/components/pages/TurnOrder/DiscardTable/Table.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+import MuiTable from '@material-ui/core/Table'
+
+const Table = styled(MuiTable)`
+  margin-top: 24px;
+  border-collapse: collapse;
+`
+
+export default Table
--- src/components/pages/TurnOrder/TurnOrderConfiguration.tsx
+++ src/components/pages/TurnOrder/TurnOrderConfiguration.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { withStyles } from '@material-ui/core/styles'
 
 import { RootState, actions, selectors } from '../../../Redux/Store'
 
