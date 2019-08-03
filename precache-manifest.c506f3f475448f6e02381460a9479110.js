dc86f04a6912e95db5a2e5026232ab48c177fc36
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-08-03 22:21:23 +0200

    Updates

---
+++ precache-manifest.c506f3f475448f6e02381460a9479110.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa90fc2cc2c530b6ff66e1debeb9bf0c",
+    "url": "/aeons-end-randomizer/index.html"
+  },
+  {
+    "revision": "34100ec579393e213e68",
+    "url": "/aeons-end-randomizer/static/css/2.927dc64a.chunk.css"
+  },
+  {
+    "revision": "34100ec579393e213e68",
+    "url": "/aeons-end-randomizer/static/js/2.4a912977.chunk.js"
+  },
+  {
+    "revision": "228b051cecd50ef46238",
+    "url": "/aeons-end-randomizer/static/js/main.a5f0cd31.chunk.js"
+  },
+  {
+    "revision": "db1f14a93e2d00853ce1",
+    "url": "/aeons-end-randomizer/static/js/runtime~main.ef7d43b0.js"
+  },
+  {
+    "revision": "bc54396771c64863193f7a3fe93d9cff",
+    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.bc543967.ttf"
+  },
+  {
+    "revision": "c243adb601cb0e965344851b1c46194f",
+    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.c243adb6.svg"
+  },
+  {
+    "revision": "cbfd0298f008558557418d19abfc2f42",
+    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.cbfd0298.eot"
+  },
+  {
+    "revision": "e31a701aa9b88125d36119acb8a10482",
+    "url": "/aeons-end-randomizer/static/media/rpgawesome-webfont.e31a701a.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aeons-end-randomizer/precache-manifest.5c45c210d614d8b1ad9faeb384785f40.js"
+  "/aeons-end-randomizer/precache-manifest.c506f3f475448f6e02381460a9479110.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/aeons-end-randomizer/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aeons-end-randomizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
