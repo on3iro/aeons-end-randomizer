571ec10066929e389ee2d9a102dad16ca776a1df
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-09-04 07:17:29 +0200

    [TASK] Refactor routing

--- src/components/App/MainApp.tsx
+++ src/components/App/MainApp.tsx
@@ -1,12 +1,35 @@
 import React, { useState, useEffect } from 'react'
-import { useRedirect } from 'hookrouter'
+import { useRedirect, useRoutes } from 'hookrouter'
 import { connect } from 'react-redux'
 
 import { RootState, actions, selectors } from '../../Redux/Store'
 
+import Nemeses from '../pages/Nemeses'
+import Mages from '../pages/Mages'
+import Supply from '../pages/Supply'
+import Settings from '../pages/Settings'
+import TurnOrder from '../pages/TurnOrder'
+
 import Content from '../Content'
 import TopBar from '../TopBar'
 import DrawerMenu from '../DrawerMenu'
+
+const createRoutingTarget = (
+  contentComponent: React.ReactNode,
+  title: string
+) => (queryParameters: Object) => ({
+  queryParameters,
+  contentComponent,
+  title,
+})
+
+const routes = {
+  '/nemesis': createRoutingTarget(<Nemeses />, 'Nemesis'),
+  '/mages': createRoutingTarget(<Mages />, 'Mages'),
+  '/supply': createRoutingTarget(<Supply />, 'Supply'),
+  '/turnorder': createRoutingTarget(<TurnOrder />, 'Turn Order'),
+  '/settings': createRoutingTarget(<Settings />, 'Settings'),
+}
 
 const mapStateToProps = (state: RootState) => ({
   isLoading: selectors.Main.ContentLoading.getIsLoading(state),
@@ -25,6 +48,7 @@ const MainApp = ({ getUserConfiguration, isLoading }: Props) => {
   // If no endpoint has been provided we redirect to the nemesis randomizer
   // because we currently do not have a dedicated landingpage
   useRedirect('/', '/nemesis')
+  const match = useRoutes(routes)
 
   const [drawerIsOpen, setDrawerIsOpen] = useState(false)
   const toggleDrawer = () => setDrawerIsOpen(!drawerIsOpen)
@@ -35,9 +59,17 @@ const MainApp = ({ getUserConfiguration, isLoading }: Props) => {
 
   return (
     <React.Fragment>
-      <TopBar drawerIsOpen={drawerIsOpen} toggleDrawer={toggleDrawer} />
+      <TopBar
+        title={match.title}
+        drawerIsOpen={drawerIsOpen}
+        toggleDrawer={toggleDrawer}
+      />
       <DrawerMenu drawerIsOpen={drawerIsOpen} toggleDrawer={toggleDrawer} />
-      <Content isLoading={isLoading} drawerIsOpen={drawerIsOpen} />
+      <Content
+        contentComponent={match.contentComponent}
+        isLoading={isLoading}
+        drawerIsOpen={drawerIsOpen}
+      />
     </React.Fragment>
   )
 }
--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,41 +1,24 @@
 import React from 'react'
-import { useRoutes } from 'hookrouter'
 
 import CircularProgress from '@material-ui/core/CircularProgress'
-
-import Nemeses from '../pages/Nemeses'
-import Mages from '../pages/Mages'
-import Supply from '../pages/Supply'
-import Settings from '../pages/Settings'
-import TurnOrder from '../pages/TurnOrder'
 
 import HeaderPlaceholder from './HeaderPlaceholder'
 import Wrapper from './Wrapper'
 
-const routes = {
-  '/': () => <div>TODO</div>,
-  '/nemesis': () => <Nemeses />,
-  '/mages': () => <Mages />,
-  '/supply': () => <Supply />,
-  '/turnorder': () => <TurnOrder />,
-  '/settings': () => <Settings />,
-}
-
 type Props = {
   isLoading: boolean
   drawerIsOpen: boolean
+  contentComponent: React.ReactNode
 }
 
-const Content = React.memo(({ isLoading, drawerIsOpen }: Props) => {
-  const match = useRoutes(routes)
-
-  return (
+const Content = React.memo(
+  ({ isLoading, drawerIsOpen, contentComponent }: Props) => (
     <Wrapper isLoading={isLoading} drawerIsOpen={drawerIsOpen}>
       <HeaderPlaceholder />
-      {isLoading ? <CircularProgress /> : match}
+      {isLoading ? <CircularProgress /> : contentComponent}
     </Wrapper>
   )
-})
+)
 
 Content.displayName = 'Content'
 
