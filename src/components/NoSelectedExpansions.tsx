260d12d41c45b3a9f58b4c7d5d08abf17763cc4f
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-09-04 07:29:49 +0200

    [FEATURE]: Add link to settings if no standalone exp selected

--- src/components/NoSelectedExpansions.tsx
+++ src/components/NoSelectedExpansions.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import A from './A'
 
 import Typography from '@material-ui/core/Typography'
 
 const NoSelectedExpansions = React.memo(() => (
   <Typography paragraph>
     There is currently no standalone set selected. Please activate at least one
-    standalone set in the settings.
+    standalone set in the settings.{' '}
+    <A href="/settings">Click here to go to the settings</A>.
   </Typography>
 ))
 
