cbfe44afb9924056944ad414df8b785d3bc3a014
Janett Hickel <janett.hickel@googlemail.com>
2019-09-08 17:34:05 +0200

    [TASK] Update styling of selected expansion config
    * Update styling of edit button next to an expansion
    * Add backlink component for config view

--- src/components/pages/Settings/Expansions/ContentCustomization/index.tsx
+++ src/components/pages/Settings/Expansions/ContentCustomization/index.tsx
@@ -5,12 +5,13 @@ import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import FormControl from '@material-ui/core/FormControl'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 
 import { RootState, selectors, actions } from '../../../../../Redux/Store'
 import * as types from '../../../../../types'
 
-import A from '../../../../A'
 import CheckboxList from '../../../../CheckboxList'
+import BackLink from './BackLink'
 
 const entitiesToItems = (
   entities: ReadonlyArray<
@@ -72,7 +73,9 @@ const ContentCustomization = React.memo(
     <Card>
       <CardContent>
         <Typography>
-          <A href="/settings">Back to settings</A>
+          <BackLink href="/settings">
+            <ChevronLeftIcon /> Back to settings
+          </BackLink>
         </Typography>
         <FormControl component={'fieldset' as 'div'}>
           <CheckboxList
