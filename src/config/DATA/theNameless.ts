1422bcf271291a7bf1e894b308612f76107d0157
Janett Hickel <janett.hickel@googlemail.com>
2019-09-24 21:51:13 +0200

    [TASK] Add mage data

--- src/config/DATA/theNameless.ts
+++ src/config/DATA/theNameless.ts
@@ -21,6 +21,30 @@ export const theNamelessData: IExpansion = {
       expansion: 'Nameless',
       name: 'Malastar',
       id: 'Malastar',
+      mageTitle: 'Breach Mage Mentor',
+      ability: `
+        <h2>Gift Of Aether</h2>
+        <p class="ability-activation">Activate during your main phase:</p>
+        <p>Gain a spell from any supply pile. You may prep that spell to 
+        any player's opened breach.</p>
+      `,
+      numberOfCharges: 6,
+      uniqueStarters: [
+        {
+          type: 'Spell',
+          name: 'Immolate',
+          expansion: 'Nameless',
+          id: 'Immolate',
+          cost: 0,
+          effect: `
+            <p>
+              While prepped, when you suffer 1 damage gain 1 charge.<br/>
+              <b>Cast:</b> Deal 1 damage.
+            </p>
+          `,
+          keywords: [],
+        },
+      ],
     },
   ],
   cards: [
