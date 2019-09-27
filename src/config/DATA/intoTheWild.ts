7e281a71103fbd6c51381b6eba0554a2e1734253
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-09-27 07:57:07 +0200

    [TASK] Add ITW and OD card data

--- src/config/DATA/intoTheWild.ts
+++ src/config/DATA/intoTheWild.ts
@@ -30,6 +30,15 @@ export const intoTheWildData: IExpansion = {
       name: 'Jeweled Brain',
       id: 'JeweledBrain',
       cost: 3,
+      effect: `
+        <p>
+          Gain 2 <span class="aether">&AElig;</span>.
+          <span class="or">OR</span>
+          Gain 1 <span class="aether">&AElig;</span>. Return to your hand a 
+          card in your discard pile that costs 0 <span class="aether">&AElig;</span>.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Relic',
@@ -37,6 +46,13 @@ export const intoTheWildData: IExpansion = {
       name: 'Blast Sphere',
       id: 'BlastSphere',
       cost: 8,
+      effect: `
+        <p>
+          Cast any player's prepped spell three times without discarding it 
+          and then destroy it.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Spell',
@@ -44,6 +60,14 @@ export const intoTheWildData: IExpansion = {
       name: 'Wound Mender',
       id: 'WoundMender',
       cost: 8,
+      effect: `
+        <p>
+          <b>Echo</b><br/>
+          <b>Cast:</b> Deal 2 damage.<br/>
+          If this was cast from an opened III or IV breach, gain 1 life.
+        </p>
+      `,
+      keywords: ['echo'],
     },
   ],
 }
--- src/config/DATA/outerDark.ts
+++ src/config/DATA/outerDark.ts
@@ -35,6 +35,13 @@ export const outerDarkData: IExpansion = {
       name: 'Char',
       id: 'Char',
       cost: 8,
+      effect: `
+        <p>
+          <b>Cast:</b> Deal 6 damage.<br/>
+          If this damage causes a minion to be discarded, any player gains 2 life.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Gem',
@@ -42,6 +49,14 @@ export const outerDarkData: IExpansion = {
       name: 'Alien Element',
       id: 'AlienElement',
       cost: 4,
+      effect: `
+        <p>
+          Gain 1 <span class="aether">&AElig;</span>.<br/>
+          For each of your breaches with a spell prepped to it, gain an 
+          additional 1 <span class="aether">&AElig;</span>.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Spell',
@@ -49,6 +64,14 @@ export const outerDarkData: IExpansion = {
       name: 'Scorch',
       id: 'Scorch',
       cost: 5,
+      effect: `
+        <p>
+          <b>Cast:</b> Deal 4 damage.<br/>
+          If this damage causes a minion from the nemesis deck to be discarded, 
+          anay ally gains 2 charges.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Spell',
@@ -56,6 +79,12 @@ export const outerDarkData: IExpansion = {
       name: 'Pyromancy',
       id: 'Pyromancy',
       cost: 7,
+      effect: `
+        <b>Cast:</b> Deal 1 damage.<br/>
+        Allies may collectively discard up to two cards in hand. For each card 
+        discarded this way, deal 3 additional damage.
+      `,
+      keywords: [],
     },
     {
       type: 'Spell',
@@ -63,6 +92,12 @@ export const outerDarkData: IExpansion = {
       name: 'Feedback Aura',
       id: 'FeedbackAura',
       cost: 5,
+      effect: `
+        <p>
+          <b>Cast:</b> Deal 3 damage.<br/>
+          If you have 4 or more charges, deal 3 additional damage.
+        </p>
+      `,
     },
     {
       type: 'Spell',
@@ -70,6 +105,13 @@ export const outerDarkData: IExpansion = {
       name: 'Catalyst',
       id: 'Catalyst',
       cost: 6,
+      effect: `
+        <p>
+          <b>Cast:</b> Deal 2 damage.<br/>
+          If you have 2 life or less, deal 5 additional damage.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Gem',
@@ -77,6 +119,14 @@ export const outerDarkData: IExpansion = {
       name: 'Pain Stone',
       id: 'PainStone',
       cost: 6,
+      effect: `
+        <p>
+          Gain 3 <span class="aether">&AElig;</span>.
+          <span class="or">OR</span>
+          Gain 2 <span class="aether">&AElig;</span> and deal 1 damage.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Relic',
@@ -84,6 +134,14 @@ export const outerDarkData: IExpansion = {
       name: 'Astral Cube',
       id: 'AstralCube',
       cost: 5,
+      effect: `
+        <p>
+          Return a gem you played this turn to your hand.<br/>
+          Reveal the top card of the turn order deck. If you revealed a 
+          player's turn order card, that player gains 1 life.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Relic',
@@ -91,6 +149,14 @@ export const outerDarkData: IExpansion = {
       name: 'Riddle Sphere',
       id: 'RiddleSphere',
       cost: 3,
+      effect: `
+        <p>
+          Gain 1 charge.
+          <span class="or">OR</span>
+          You may lose 2 charges. If you do, gain 5 <span class="aether">&AElig;</span>.
+        </p>
+      `,
+      keywords: [],
     },
     {
       type: 'Spell',
@@ -98,6 +164,15 @@ export const outerDarkData: IExpansion = {
       name: 'Nether Conduit',
       id: 'NetherConduit',
       cost: 7,
+      effect: `
+        <b>Cast:<b/> Reveal a card in hand that costs 2 <span class="aether">&AElig;</span> 
+        or more. If you do, deal damage equal to the number of cards missing in 
+        that card's supply pile. Then, any ally may gain a card from that supply
+        pile.<br/>
+        <span class="hint">(Gem supply piles start with 7 cards. Relic and 
+        spell supply piles start with 5 cards)</span>
+      `,
+      keywords: [],
     },
     {
       type: 'Gem',
@@ -105,6 +180,14 @@ export const outerDarkData: IExpansion = {
       name: 'Haunted Berylite',
       id: 'HauntedBerylite',
       cost: 3,
+      effect: `
+        <p>
+          Gain 2 <span class="aether">&AElig;</span>.
+          <span class="or">OR</span>
+          Discard a card in hand. If you do, gain 2 charges.
+        </p>
+      `,
+      keywords: [],
     },
   ],
 }
