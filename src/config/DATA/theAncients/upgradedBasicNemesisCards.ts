420f1ff5066c3e0197f4e44464c5b38625cc3258
Janett Hickel <janett.hickel@googlemail.com>
2020-02-15 13:17:46 +0100

    [TASK] Update data of upgraded nemesis cards

--- src/config/DATA/theAncients/upgradedBasicNemesisCards.ts
+++ src/config/DATA/theAncients/upgradedBasicNemesisCards.ts
@@ -7,6 +7,12 @@ export const upgradedBasicNemesisCards: UpgradedBasicNemesisCard[] = [
     expansion: 'TA',
     tier: 1,
     type: 'Attack',
+    effect: `
+      <p>
+        Unleash. Any player suffers 4 damage.
+      </p>
+    `,
+    upgraded: true,
   },
   {
     id: 'FurrowOfDestruction',
@@ -14,6 +20,15 @@ export const upgradedBasicNemesisCards: UpgradedBasicNemesisCard[] = [
     expansion: 'TA',
     tier: 1,
     type: 'Power',
+    power: 2,
+    effect: `
+      <p>
+        <b>To Discard:</b> Spend 6 <span class="aether">&AElig;</span>.<br />
+        <br />
+        <b>Power 2:</b> Unleash twice. Gravehold suffers 3 damage.
+      </p>
+    `,
+    upgraded: true,
   },
   {
     id: 'Mangle',
@@ -21,6 +36,12 @@ export const upgradedBasicNemesisCards: UpgradedBasicNemesisCard[] = [
     expansion: 'TA',
     tier: 2,
     type: 'Attack',
+    effect: `
+      <p>
+        The player with the most charges loses 3 charges and suffers 3 damage. A different player discards three cards in hand.
+      </p>
+    `,
+    upgraded: true,
   },
   {
     id: 'HowlingSlayer',
@@ -28,5 +49,12 @@ export const upgradedBasicNemesisCards: UpgradedBasicNemesisCard[] = [
     expansion: 'TA',
     tier: 2,
     type: 'Minion',
+    hp: 14,
+    effect: `
+      <p>
+        <b>Persistent:</b> Any player suffers 2 damage. Any player discards a card in hand.
+      </p>
+    `,
+    upgraded: true,
   },
 ]
