6cd3d40b5483161818a43c596246247c6640244e
Janett Hickel <janett.hickel@googlemail.com>
2020-02-15 20:16:48 +0100

    [TASK] Update redux store for treasures and upgraded basic nemesis cards

--- src/Redux/Store/Settings/Expansions/UpgradedBasicNemesisCards.ts
+++ src/Redux/Store/Settings/Expansions/UpgradedBasicNemesisCards.ts
@@ -1,22 +1,39 @@
 import { createAction, ActionsUnion } from '@martin_hotell/rex-tils'
-import { LoopReducer } from 'redux-loop'
+import { LoopReducer, loop, Cmd } from 'redux-loop'
 import { createSelector } from 'reselect'
+import { get as getFromDb, set as setToDb } from 'idb-keyval'
 
-import * as types from '../../../../types'
-import config from '../../../../config'
-import { RootState } from '../..'
+import * as types from 'types'
+import config from 'config'
+
+import { RootState } from 'Redux/Store'
+
+const UPGRADED_BASIC_NEMESIS_CARDS_DB_KEY = 'upgradedBasicNemesisCards-1.8'
 
 ///////////
 // STATE //
 ///////////
 
+type SelectedUpgradedBasicNemesisCards = {
+  [key: string]: types.UpgradedBasicNemesisCard & { selected: boolean }
+}
+
 export type State = Readonly<{
-  upgradedBasicNemesisCards: types.UpgradedBasicNemesisCards
+  upgradedBasicNemesisCards: SelectedUpgradedBasicNemesisCards
   upgradedBasicNemesisCardIds: string[]
 }>
 
 export const initialState: State = {
-  upgradedBasicNemesisCards: config.NORMALIZEDDATA.upgradedBasicNemesisCards,
+  upgradedBasicNemesisCards: config.NORMALIZEDDATA.upgradedBasicNemesisCardIds.reduce(
+    (acc, id) => ({
+      ...acc,
+      [id]: {
+        ...config.NORMALIZEDDATA.upgradedBasicNemesisCards[id],
+        selected: true,
+      },
+    }),
+    {}
+  ),
   upgradedBasicNemesisCardIds:
     config.NORMALIZEDDATA.upgradedBasicNemesisCardIds,
 }
@@ -25,9 +42,27 @@ export const initialState: State = {
 // ACTIONS //
 /////////////
 
-export enum ActionTypes {}
+export enum ActionTypes {
+  TOGGLE_CARD = 'Settings/Expansions/UpgradedBasicNemesisCards/TOGGLE_CARD',
+  SET_TO_DB = 'Settings/Expansions/UpgradedBasicNemesisCards/SET_TO_DB',
+  SET_TO_DB_SUCCESS = 'Settings/Expansions/UpgradedBasicNemesisCards/SET_TO_DB_SUCCESS',
+  SET_TO_DB_FAILURE = 'Settings/Expansions/UpgradedBasicNemesisCards/SET_TO_DB_FAILURE',
+  FETCH_FROM_DB = 'Settings/Expansions/UpgradedBasicNemesisCards/FETCH_FROM_DB',
+  FETCH_FROM_DB_SUCCESS = 'Settings/Expansions/UpgradedBasicNemesisCards/FETCH_FROM_DB_SUCCESS',
+  FETCH_FROM_DB_FAILURE = 'Settings/Expansions/UpgradedBasicNemesisCards/FETCH_FROM_DB_FAILURE',
+}
 
 export const actions = {
+  toggleCard: (id: string) => createAction(ActionTypes.TOGGLE_CARD, id),
+  setToDB: (state: State) => createAction(ActionTypes.SET_TO_DB, state),
+  setToDBSuccessful: () => createAction(ActionTypes.SET_TO_DB_SUCCESS),
+  setToDBFailed: (error: Object) =>
+    createAction(ActionTypes.SET_TO_DB_FAILURE, error),
+  fetchFromDB: () => createAction(ActionTypes.FETCH_FROM_DB),
+  fetchFromDBSuccessful: (selectedCards: string[]) =>
+    createAction(ActionTypes.FETCH_FROM_DB_SUCCESS, selectedCards),
+  fetchFromDBFailed: (error: Object) =>
+    createAction(ActionTypes.FETCH_FROM_DB_FAILURE, error),
   noOp: () => createAction('NOOP'),
 }
 
@@ -42,6 +77,70 @@ export const Reducer: LoopReducer<State, Action> = (
   action: Action
 ) => {
   switch (action.type) {
+    case ActionTypes.TOGGLE_CARD: {
+      const newState = {
+        ...state,
+        upgradedBasicNemesisCards: {
+          ...state.upgradedBasicNemesisCards,
+          [action.payload]: {
+            ...state.upgradedBasicNemesisCards[action.payload],
+            selected: !state.upgradedBasicNemesisCards[action.payload].selected,
+          },
+        },
+      }
+
+      const selectedCardsToSave = state.upgradedBasicNemesisCardIds.filter(
+        id => newState.upgradedBasicNemesisCards[id].selected
+      )
+
+      return loop(
+        newState,
+        Cmd.run(setToDb, {
+          args: [UPGRADED_BASIC_NEMESIS_CARDS_DB_KEY, selectedCardsToSave],
+          successActionCreator: actions.setToDBSuccessful,
+          failActionCreator: actions.setToDBFailed,
+        })
+      )
+    }
+
+    case ActionTypes.FETCH_FROM_DB: {
+      return loop(
+        state,
+        Cmd.run(getFromDb, {
+          args: [UPGRADED_BASIC_NEMESIS_CARDS_DB_KEY],
+          successActionCreator: actions.fetchFromDBSuccessful,
+          failActionCreator: actions.fetchFromDBFailed,
+        })
+      )
+    }
+
+    case ActionTypes.FETCH_FROM_DB_SUCCESS: {
+      if (!action.payload) {
+        return initialState
+      }
+
+      const selectedCards: string[] = action.payload
+      const newState = Object.values(state.upgradedBasicNemesisCards).reduce(
+        (acc, upgradedBasicNemesisCard) => ({
+          ...acc,
+          upgradedBasicNemesisCards: {
+            ...acc.upgradedBasicNemesisCards,
+            [upgradedBasicNemesisCard.id]: {
+              ...upgradedBasicNemesisCard,
+              selected: selectedCards.includes(upgradedBasicNemesisCard.id),
+            },
+          },
+        }),
+        state
+      )
+
+      return newState || initialState
+    }
+
+    case ActionTypes.FETCH_FROM_DB_FAILURE: {
+      return state
+    }
+
     default: {
       return state
     }
@@ -51,6 +150,8 @@ export const Reducer: LoopReducer<State, Action> = (
 ///////////////
 // SELECTORS //
 ///////////////
+
+// All
 
 const getUpgradedBasicNemesisCards = (state: RootState) =>
   state.Settings.Expansions.UpgradedBasicNemesisCards.upgradedBasicNemesisCards
@@ -89,10 +190,25 @@ const getUpgradedBasicNemesisCardList = createSelector(
   (cards, ids) => ids.map(id => cards[id])
 )
 
+// Selected
+
+const getSelectedUpgradedBasicNemesisCardIds = createSelector(
+  [getUpgradedBasicNemesisCards, getUpgradedBasicNemesisCardIds],
+  (upgradedBasicNemesisCards, ids) =>
+    ids.filter(id => upgradedBasicNemesisCards[id].selected)
+)
+
+const getSelectedUpgradedBasicNemesisCards = createSelector(
+  [getUpgradedBasicNemesisCards, getSelectedUpgradedBasicNemesisCardIds],
+  (upgradedBasicNemesisCards, ids) =>
+    ids.map(id => upgradedBasicNemesisCards[id])
+)
+
 export const selectors = {
   getUpgradedBasicNemesisCards,
   getUpgradedBasicNemesisCardIds,
   getUpgradedBasicNemesisCardIdsByExpansionId,
   getUpgradedBasicNemesisCardsByExpansionId,
   getUpgradedBasicNemesisCardList,
+  getSelectedUpgradedBasicNemesisCards,
 }
