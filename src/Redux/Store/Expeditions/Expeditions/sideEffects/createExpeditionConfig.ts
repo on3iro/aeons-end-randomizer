b135c591b01cccd5f28484004b2924831f001411
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-02-16 13:36:09 +0100

    FEATURE: Add settignsSnapshot-migrations
    This should make sure that we are backwards compatible to expeditions
    created before the release of our settingsSnapshot-feature.

--- src/Redux/Store/Expeditions/Expeditions/sideEffects/createExpeditionConfig.ts
+++ src/Redux/Store/Expeditions/Expeditions/sideEffects/createExpeditionConfig.ts
@@ -12,40 +12,7 @@ import {
 import * as types from 'types'
 
 import { BaseConfig } from '../types'
-
-const createSettingsSnapshot = (
-  state: RootState,
-  marketId: string
-): types.SettingsSnapshot => {
-  const getCustomAndPredefined = selectors.Settings.SupplySetups.makeGetCustomAndPredefined()
-  const allSupplySetups = getCustomAndPredefined(state)
-
-  const supplySetup = allSupplySetups[marketId]
-  const availableCardIds = selectors.Settings.Expansions.getSelectedCardIdsForSelectedExpansions(
-    state
-  )
-  const availableMageIds = selectors.Settings.Expansions.getSelectedMageIdsForSelectedExpansions(
-    state
-  )
-  const availableNemesisIds = selectors.Settings.Expansions.getSelectedNemesisIdsForSelectedExpansions(
-    state
-  )
-  const availableTreasureIds = selectors.Settings.Expansions.getTreasureIdsForSelectedExpansions(
-    state
-  )
-  const availableUpgradedBasicNemesisCardIds = selectors.Settings.Expansions.getUpgradedBasicNemesisCardIdsForSelectedExpansions(
-    state
-  )
-
-  return {
-    supplySetup,
-    availableCardIds,
-    availableMageIds,
-    availableNemesisIds,
-    availableTreasureIds,
-    availableUpgradedBasicNemesisCardIds,
-  }
-}
+import { createSettingsSnapshot } from './createSettingsSnapshot'
 
 const createSupplyIds = (
   state: RootState,
