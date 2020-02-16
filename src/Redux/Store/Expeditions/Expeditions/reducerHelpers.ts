b458fd080bb622528e263e912316bbcab38a28fb
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-02-16 13:36:09 +0100

    FEATURE: Add settignsSnapshot-migrations
    This should make sure that we are backwards compatible to expeditions
    created before the release of our settingsSnapshot-feature.

--- src/Redux/Store/Expeditions/Expeditions/reducerHelpers.ts
+++ src/Redux/Store/Expeditions/Expeditions/reducerHelpers.ts
@@ -396,3 +396,47 @@ export const fetchFromDB = (state: State, _: Action) => {
     })
   )
 }
+
+export const migrateAfterFetch = (
+  action: ReturnType<typeof actions.fetchFromDBSuccessful>
+) => {
+  const expeditions = action.payload.expeditionIds.map(
+    id => action.payload.expeditions[id]
+  )
+  const expeditionsToMigrate = expeditions.filter(expedition => {
+    return !expedition.settingsSnapshot || !expedition.seed
+  })
+
+  if (expeditionsToMigrate.length === 0) {
+    return action.payload
+  }
+
+  return loop(
+    action.payload,
+    Cmd.run(sideEffects.migrateToSettingsSnapshot, {
+      args: [
+        Cmd.getState,
+        {
+          state: action.payload,
+          expeditionsToMigrate,
+        },
+      ],
+      successActionCreator: actions.migrateToSettingsSnapshotSuccessful,
+    })
+  )
+}
+
+export const migrateAfterFetchSuccess = (
+  state: State,
+  action: ReturnType<typeof actions.migrateToSettingsSnapshotSuccessful>
+) => {
+  const newState = action.payload || state
+  return loop(
+    newState,
+    Cmd.run(setToDb, {
+      args: [EXPEDITIONS_DB_KEY, newState],
+      successActionCreator: actions.setToDBSuccessful,
+      failActionCreator: actions.setToDBFailed,
+    })
+  )
+}
