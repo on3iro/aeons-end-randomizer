218f15eca0dea768e2d0f7c276738037ddc2b372
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-02-19 16:28:20 +0100

    TASK: Fix broken tests

--- src/Redux/_test_/helpers/createIdList.test.ts
+++ src/Redux/_test_/helpers/createIdList.test.ts
@@ -1,51 +1,15 @@
 import { createIdList, createArrayWithDefaultValues } from '../../helpers'
-
-const getFirst = (available: any[]) => available[0]
+import { getRandomEntity } from 'Redux/helpers'
 
 describe('createIdList()', () => {
   test.each([
-    [
-      ['1', '2', '3', '4'],
-      createArrayWithDefaultValues(1, 'EMPTY'),
-      getFirst,
-      {
-        availableEntities: ['2', '3', '4'],
-        result: ['1'],
-      },
-    ],
-    [
-      ['1', '2', '3', '4'],
-      createArrayWithDefaultValues(3, 'EMPTY'),
-      getFirst,
-      {
-        availableEntities: ['4'],
-        result: ['1', '2', '3'],
-      },
-    ],
-    [
-      ['1', '2', '3', '4'],
-      createArrayWithDefaultValues(4, 'EMPTY'),
-      getFirst,
-      {
-        availableEntities: [],
-        result: ['1', '2', '3', '4'],
-      },
-    ],
-    [
-      ['1', '2', '3'],
-      createArrayWithDefaultValues(4, 'EMPTY'),
-      getFirst,
-      {
-        availableEntities: [],
-        result: ['1', '2', '3'],
-      },
-    ],
-  ])(
-    'createIdList(%o, %o, %o)',
-    (availableEntities, slots, entityGetter, expected) => {
-      expect(createIdList(availableEntities, slots, entityGetter)).toEqual(
-        expected
-      )
-    }
-  )
+    [['1', '2', '3', '4'], createArrayWithDefaultValues(1, 'EMPTY')],
+    [['1', '2', '3', '4'], createArrayWithDefaultValues(3, 'EMPTY')],
+    [['1', '2', '3', '4'], createArrayWithDefaultValues(4, 'EMPTY')],
+    [['1', '2', '3'], createArrayWithDefaultValues(4, 'EMPTY')],
+  ])('createIdList(%o, %o, %o)', (availableEntities, slots) => {
+    expect(
+      createIdList(availableEntities, slots, getRandomEntity, { seed: 'test' })
+    ).toMatchSnapshot()
+  })
 })
