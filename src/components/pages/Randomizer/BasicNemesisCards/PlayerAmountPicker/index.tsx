ea12fdcac7dd69984abddbbb65b9fdd91f8b843d
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-03-01 10:40:19 +0100

    REFACTOR: Amountpickers into its own component
    While we only duplicated this component once, it is so simple, that its
    unlikely that we need to rebuild it. Therefore it might make sense to
    extract it into its own component. Which is what I have done in this
    commit. In general we do not want to abstract permaturely, though.

--- src/components/pages/Randomizer/BasicNemesisCards/PlayerAmountPicker/index.tsx
+++ src/components/pages/Randomizer/BasicNemesisCards/PlayerAmountPicker/index.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 
-import Radio from '@material-ui/core/Radio'
-import RadioGroup from '@material-ui/core/RadioGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import FormControl from '@material-ui/core/FormControl'
-import FormLabel from '@material-ui/core/FormLabel'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
+import AmountPicker from 'components/molecules/AmountPicker'
 
 type Props = {
   selectedValue: string
@@ -15,25 +9,12 @@ type Props = {
 
 const PlayerAmountPicker = ({ selectedValue, handleAmountChange }: Props) => {
   return (
-    <Card>
-      <CardContent>
-        <FormControl component={'fieldset' as 'div'}>
-          <FormLabel>Amount of Players</FormLabel>
-          <RadioGroup
-            aria-label="Amount of Players"
-            name="playerAmount"
-            value={selectedValue}
-            onChange={handleAmountChange}
-            row={true}
-          >
-            <FormControlLabel value="1" control={<Radio />} label="1" />
-            <FormControlLabel value="2" control={<Radio />} label="2" />
-            <FormControlLabel value="3" control={<Radio />} label="3" />
-            <FormControlLabel value="4" control={<Radio />} label="4" />
-          </RadioGroup>
-        </FormControl>
-      </CardContent>
-    </Card>
+    <AmountPicker
+      label="Amount of Players"
+      selectedValue={selectedValue}
+      handleAmountChange={handleAmountChange}
+      optionsCount={4}
+    />
   )
 }
 
