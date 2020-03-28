cb1f718349620e178a2f584eaecd95d54496c868
Janett Hickel <janett.hickel@googlemail.com>
2020-03-28 18:21:08 +0100

    REFACTOR: SupplySetups store

---
+++ src/Redux/Store/Settings/SupplySetups/constants.ts
@@ -0,0 +1,3 @@
+const SUPPLY_DB_KEY = 'supplySetups-1.6' // FIXME this is a quickfix, see https://github.com/on3iro/aeons-end-randomizer/issues/138
+
+export { SUPPLY_DB_KEY }
---
+++ src/Redux/Store/Settings/SupplySetups/reducerHelpers/toggleAll.ts
@@ -0,0 +1,35 @@
+import { State } from '../types'
+import { newStateWithDBWrite, allSetsAreSelected } from '../helpers'
+
+export const toggleAll = (state: State) => {
+  const allSetsSelected = allSetsAreSelected(state)
+  const updatedPredefined = Object.values(state.Predefined.setups).reduce(
+    (acc, setup) => ({
+      ...acc,
+      [setup.id]: {
+        ...setup,
+        active: setup.default ? true : !allSetsSelected,
+      },
+    }),
+    {}
+  )
+  const updatedCustom = Object.values(state.Custom.setups).reduce(
+    (acc, setup) => ({
+      ...acc,
+      [setup.id]: { ...setup, active: !allSetsSelected },
+    }),
+    {}
+  )
+  const newState = {
+    Predefined: {
+      ...state.Predefined,
+      setups: updatedPredefined,
+    },
+    Custom: {
+      ...state.Custom,
+      setups: updatedCustom,
+    },
+  }
+
+  return newStateWithDBWrite(newState)
+}
