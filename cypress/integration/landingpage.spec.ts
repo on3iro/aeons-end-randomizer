f3ef3bbaa7a67328e1dc258900e87e5baf2317a0
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-03-30 21:55:13 +0200

    TASK: Add basic cypress setup incl. CI

---
+++ cypress/integration/landingpage.spec.ts
@@ -0,0 +1,11 @@
+describe('The landing page "/"', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
+  })
+
+  it('redirects to the nemesis randomizer', () => {
+    cy.location().should(loc => {
+      expect(loc.href).to.eq('http://localhost:3000/randomizer/nemesis')
+    })
+  })
+})
