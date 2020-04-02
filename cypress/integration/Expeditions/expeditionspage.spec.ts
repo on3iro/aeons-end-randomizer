0b73184211571a78bce38f28c3b0d32431f941b0
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-04-02 14:54:17 +0200

    FIX: Test interdependencies

--- cypress/integration/Expeditions/expeditionspage.spec.ts
+++ cypress/integration/Expeditions/expeditionspage.spec.ts
@@ -8,6 +8,13 @@ describe('Expedition page', () => {
     cy.visit('expeditions')
   })
 
+  after(() => {
+    // @ts-ignore disable-line
+    cy.deselectAllExpansions()
+    // @ts-ignore disable-line
+    cy.cleanupIndexedDB()
+  })
+
   it('successfully loads', () => {
     // cy.visit('/expeditions')
     cy.get('h6').contains('Expeditions')
