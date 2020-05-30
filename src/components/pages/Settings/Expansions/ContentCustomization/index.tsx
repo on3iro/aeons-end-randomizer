ce136b5fb4535f93dcf4bcdb48a7888bd4b0ec8c
Janett Hickel <janett.hickel@googlemail.com>
2020-05-30 13:27:18 +0200

    TASK: Styling language select

--- src/components/pages/Settings/Expansions/ContentCustomization/index.tsx
+++ src/components/pages/Settings/Expansions/ContentCustomization/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
+
+import { RootState, selectors, actions } from 'Redux/Store'
+import {
+  LANGUAGE_KEYS,
+  LanguageKey,
+} from 'Redux/Store/Settings/Expansions/types'
 
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
-import { FormGroup, MenuItem } from '@material-ui/core'
+import { FormGroup, MenuItem, InputLabel } from '@material-ui/core'
 import FormControl from '@material-ui/core/FormControl'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Tooltip from '@material-ui/core/Tooltip'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import InfoIcon from '@material-ui/icons/Info'
 
 import BackLink from 'components/molecules/BackLink'
+import IconButton from 'components/atoms/IconButton'
+import SelectField from 'components/atoms/SelectField'
 
+import Wrapper from './Wrapper'
+import SelectWithTooltipWrapper from './SelectWithTooltipWrapper'
 import Nemeses from './Nemeses'
 import Mages from './Mages'
 import Gems from './Gems'
@@ -17,12 +29,6 @@ import Spells from './Spells'
 import BasicNemesisCards from './BasicNemesisCards'
 import UpgradedBasicNemesisCards from './UpgradedBasicNemesisCards'
 import Treasures from './Treasures'
-import SelectField from 'components/atoms/SelectField'
-import { RootState, selectors, actions } from 'Redux/Store'
-import {
-  LANGUAGE_KEYS,
-  LanguageKey,
-} from 'Redux/Store/Settings/Expansions/types'
 
 type OwnProps = { expansionId: string }
 
@@ -44,54 +50,92 @@ type Props = ReturnType<typeof mapStateToProps> &
   typeof mapDispatchToProps &
   OwnProps
 
-const ContentCustomization = ({ expansionId, lang, selectLanguage }: Props) => (
-  <Card>
-    <CardContent>
-      <BackLink to="/settings" label="Back to settings" />
-      <FormControl component={'fieldset' as 'div'}>
-        <FormGroup style={{ marginBottom: '20px' }}>
-          <FormControlLabel
-            label="Language"
-            control={
-              <SelectField
-                key="lang"
-                value={lang}
-                onChange={(
-                  e: React.ChangeEvent<{ name?: string; value: LanguageKey }>
-                ) => {
-                  selectLanguage(expansionId, e.target.value)
-                }}
-                inputProps={{
-                  name: 'lang',
-                  id: `lang-${lang}`,
-                }}
-                data-test="select-type"
-              >
-                {LANGUAGE_KEYS.map(language => (
-                  <MenuItem
-                    key={language}
-                    value={language}
-                    data-test="select-type-menu-item"
+const ContentCustomization = ({ expansionId, lang, selectLanguage }: Props) => {
+  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false)
+
+  const handleTooltipClose = () => {
+    setTooltipOpen(false)
+  }
+
+  const handleTooltipOpen = () => {
+    setTooltipOpen(!tooltipOpen)
+  }
+
+  return (
+    <Card>
+      <CardContent>
+        <Wrapper>
+          <BackLink to="/settings" label="Back to settings" />
+          <FormControl component={'fieldset' as 'div'}>
+            <SelectWithTooltipWrapper>
+              <FormGroup style={{ minWidth: '50%' }}>
+                <InputLabel shrink>Language</InputLabel>
+                <SelectField
+                  key="lang"
+                  value={lang}
+                  onChange={(
+                    e: React.ChangeEvent<{ name?: string; value: LanguageKey }>
+                  ) => {
+                    selectLanguage(expansionId, e.target.value)
+                  }}
+                  inputProps={{
+                    name: 'lang',
+                    id: `lang-${lang}`,
+                  }}
+                  data-test="select-type"
+                >
+                  {LANGUAGE_KEYS.map(language => (
+                    <MenuItem
+                      key={language}
+                      value={language}
+                      data-test="select-type-menu-item"
+                    >
+                      {language}
+                    </MenuItem>
+                  ))}
+                </SelectField>
+              </FormGroup>
+              <ClickAwayListener onClickAway={handleTooltipClose}>
+                <Tooltip
+                  title="Note: Content without available translations will fallback to english!"
+                  PopperProps={{
+                    disablePortal: true,
+                    modifiers: {
+                      arrow: {
+                        enabled: true,
+                      },
+                    },
+                  }}
+                  onClose={handleTooltipClose}
+                  open={tooltipOpen}
+                  disableFocusListener
+                  disableHoverListener
+                  disableTouchListener
+                >
+                  <IconButton
+                    color="primary"
+                    aria-label="Show language select info"
+                    onClick={handleTooltipOpen}
                   >
-                    {language}
-                  </MenuItem>
-                ))}
-              </SelectField>
-            }
-          />
-        </FormGroup>
-        <Nemeses expansionId={expansionId} />
-        <Mages expansionId={expansionId} />
-        <Gems expansionId={expansionId} />
-        <Relics expansionId={expansionId} />
-        <Spells expansionId={expansionId} />
-        <BasicNemesisCards expansionId={expansionId} />
-        <UpgradedBasicNemesisCards expansionId={expansionId} />
-        <Treasures expansionId={expansionId} />
-      </FormControl>
-    </CardContent>
-  </Card>
-)
+                    <InfoIcon />
+                  </IconButton>
+                </Tooltip>
+              </ClickAwayListener>
+            </SelectWithTooltipWrapper>
+            <Nemeses expansionId={expansionId} />
+            <Mages expansionId={expansionId} />
+            <Gems expansionId={expansionId} />
+            <Relics expansionId={expansionId} />
+            <Spells expansionId={expansionId} />
+            <BasicNemesisCards expansionId={expansionId} />
+            <UpgradedBasicNemesisCards expansionId={expansionId} />
+            <Treasures expansionId={expansionId} />
+          </FormControl>
+        </Wrapper>
+      </CardContent>
+    </Card>
+  )
+}
 
 export default connect(
   mapStateToProps,
