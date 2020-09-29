376f0aae107aed41d188e58dad8734df0e08d837
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-09-29 09:02:10 +0200

    wip

--- src/components/App/MainApp.tsx
+++ src/components/App/MainApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { connect } from 'react-redux'
 
 import { RootState, actions, selectors } from '../../Redux/Store'
@@ -6,6 +6,9 @@ import { RootState, actions, selectors } from '../../Redux/Store'
 import Content from '../organisms/Content'
 import TopBar from '../organisms/TopBar'
 import DrawerMenu from '../organisms/DrawerMenu'
+
+import * as serviceWorker from '../../serviceWorker'
+import { Snackbar } from '@material-ui/core'
 
 const mapStateToProps = (state: RootState) => ({
   isLoading: selectors.Main.ContentLoading.getIsLoading(state),
@@ -18,6 +21,40 @@ const mapDispatchToProps = {
 type Props = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps & {}
 
 const MainApp = ({ getUserConfiguration, isLoading }: Props) => {
+  const [waitingWorker, setWaitingWorker] = useState<Worker | undefined>(
+    undefined
+  )
+  const [newVersionAvailable, setNewVersionAvailable] = useState(false)
+  const [showSnackBar, setShowSnackBar] = useState(false)
+
+  const onServiceWorkerUpdate = (registration: {
+    waiting: Worker
+    postMessage: Function
+  }) => {
+    if (registration && registration.waiting) {
+      setWaitingWorker(registration.waiting)
+    }
+
+    setNewVersionAvailable(true)
+  }
+
+  const updateServiceWorker = useCallback(() => {
+    if (waitingWorker) {
+      waitingWorker.postMessage({ type: 'SKIP_WAITING' })
+      setNewVersionAvailable(false)
+      window.location.reload()
+    }
+  }, [waitingWorker, setNewVersionAvailable])
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      serviceWorker.register({ onUpdate: onServiceWorkerUpdate })
+    }
+
+    if (newVersionAvailable) {
+      setShowSnackBar(true)
+    }
+  }, [])
   const [drawerIsOpen, setDrawerIsOpen] = useState(false)
   const toggleDrawer = () => setDrawerIsOpen(!drawerIsOpen)
 
@@ -30,6 +67,23 @@ const MainApp = ({ getUserConfiguration, isLoading }: Props) => {
       <TopBar drawerIsOpen={drawerIsOpen} toggleDrawer={toggleDrawer} />
       <DrawerMenu drawerIsOpen={drawerIsOpen} toggleDrawer={toggleDrawer} />
       <Content isLoading={isLoading} />
+      <Snackbar
+        open={showSnackBar}
+        autoHideDuration={undefined}
+        onClose={updateServiceWorker}
+      >
+        <>
+          <button
+            onClick={() => {
+              setShowSnackBar(false)
+              updateServiceWorker()
+            }}
+          >
+            close
+          </button>
+          <p>New update available</p>
+        </>
+      </Snackbar>
       <div id="modal-root" />
     </React.Fragment>
   )
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import { ThemeProvider } from 'styled-components/macro'
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -12,45 +12,7 @@ import { mainTheme, muiTheme } from '../../mainTheme'
 import MainApp from './MainApp'
 import Wrapper from './Wrapper'
 
-import * as serviceWorker from '../../serviceWorker'
-import { Snackbar } from '@material-ui/core'
-
 const App = () => {
-  const [waitingWorker, setWaitingWorker] = useState<Worker | undefined>(
-    undefined
-  )
-  const [newVersionAvailable, setNewVersionAvailable] = useState(true)
-  const [showSnackBar, setShowSnackBar] = useState(false)
-
-  const onServiceWorkerUpdate = (registration: {
-    waiting: Worker
-    postMessage: Function
-  }) => {
-    if (registration && registration.waiting) {
-      setWaitingWorker(registration.waiting)
-    }
-
-    setNewVersionAvailable(true)
-  }
-
-  const updateServiceWorker = useCallback(() => {
-    if (waitingWorker) {
-      waitingWorker.postMessage({ type: 'SKIP_WAITING' })
-      setNewVersionAvailable(false)
-      window.location.reload()
-    }
-  }, [waitingWorker, setNewVersionAvailable])
-
-  useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      serviceWorker.register({ onUpdate: onServiceWorkerUpdate })
-    }
-
-    if (newVersionAvailable) {
-      setShowSnackBar(true)
-    }
-  }, [])
-
   return (
     <ThemeProvider theme={mainTheme}>
       <MuiThemeProvider theme={muiTheme}>
@@ -58,21 +20,6 @@ const App = () => {
           <Wrapper>
             <CssBaseline />
             <MainApp />
-            <Snackbar
-              open={showSnackBar}
-              autoHideDuration={undefined}
-              onClose={updateServiceWorker}
-            >
-              <button
-                onClick={() => {
-                  setShowSnackBar(false)
-                  updateServiceWorker()
-                }}
-              >
-                close
-              </button>
-              <p>New update available</p>
-            </Snackbar>
           </Wrapper>
         </Router>
       </MuiThemeProvider>
