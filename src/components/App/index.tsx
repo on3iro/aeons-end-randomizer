93c1d1e6b0e5ca14ae8f879e08a1082e7d423d73
Theo Salzmann <theo.salzmann@sandstorm.de>
2020-09-29 08:56:34 +0200

    wip

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { ThemeProvider } from 'styled-components/macro'
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -12,17 +12,72 @@ import { mainTheme, muiTheme } from '../../mainTheme'
 import MainApp from './MainApp'
 import Wrapper from './Wrapper'
 
-const App = () => (
-  <ThemeProvider theme={mainTheme}>
-    <MuiThemeProvider theme={muiTheme}>
-      <Router>
-        <Wrapper>
-          <CssBaseline />
-          <MainApp />
-        </Wrapper>
-      </Router>
-    </MuiThemeProvider>
-  </ThemeProvider>
-)
+import * as serviceWorker from '../../serviceWorker'
+import { Snackbar } from '@material-ui/core'
+
+const App = () => {
+  const [waitingWorker, setWaitingWorker] = useState<Worker | undefined>(
+    undefined
+  )
+  const [newVersionAvailable, setNewVersionAvailable] = useState(true)
+  const [showSnackBar, setShowSnackBar] = useState(false)
+
+  const onServiceWorkerUpdate = (registration: {
+    waiting: Worker
+    postMessage: Function
+  }) => {
+    if (registration && registration.waiting) {
+      setWaitingWorker(registration.waiting)
+    }
+
+    setNewVersionAvailable(true)
+  }
+
+  const updateServiceWorker = useCallback(() => {
+    if (waitingWorker) {
+      waitingWorker.postMessage({ type: 'SKIP_WAITING' })
+      setNewVersionAvailable(false)
+      window.location.reload()
+    }
+  }, [waitingWorker, setNewVersionAvailable])
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      serviceWorker.register({ onUpdate: onServiceWorkerUpdate })
+    }
+
+    if (newVersionAvailable) {
+      setShowSnackBar(true)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <MuiThemeProvider theme={muiTheme}>
+        <Router>
+          <Wrapper>
+            <CssBaseline />
+            <MainApp />
+            <Snackbar
+              open={showSnackBar}
+              autoHideDuration={undefined}
+              onClose={updateServiceWorker}
+            >
+              <button
+                onClick={() => {
+                  setShowSnackBar(false)
+                  updateServiceWorker()
+                }}
+              >
+                close
+              </button>
+              <p>New update available</p>
+            </Snackbar>
+          </Wrapper>
+        </Router>
+      </MuiThemeProvider>
+    </ThemeProvider>
+  )
+}
 
 export default React.memo(App)
--- src/index.tsx
+++ src/index.tsx
@@ -40,4 +40,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // @ts-ignore
-serviceWorker.register()
+// serviceWorker.register()
