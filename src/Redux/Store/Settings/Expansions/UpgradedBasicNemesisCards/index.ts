cc6d6eff37e69ee8feacde2b12e8c8070ceb89dc
Janett Hickel <janett.hickel@googlemail.com>
2020-10-05 20:59:26 +0200

    FEATURE: De-/Select all entities in expansion

--- src/Redux/Store/Settings/Expansions/UpgradedBasicNemesisCards/index.ts
+++ src/Redux/Store/Settings/Expansions/UpgradedBasicNemesisCards/index.ts
@@ -1,14 +1,25 @@
-import { combineReducers } from 'redux-loop'
+import { combineReducers, reduceReducers, loop, Cmd } from 'redux-loop'
 import { createSelector } from 'reselect'
+import { createAction, ActionsUnion } from '@martin_hotell/rex-tils'
+import { set as setToDb } from 'idb-keyval'
+
+import {
+  getEntitiesByIdListWithLanguageFallback,
+  getContentByIdWithLanguageFallback,
+} from '../helpers'
 import { selectors as LanguageSelectors } from '../Languages'
+import {
+  Action as ExpansionAction,
+  ActionTypes as ExpansionActionTypes,
+} from '../'
 
 import * as Content from './content'
 import * as Selected from './selected'
 import * as Ids from './ids'
-import {
-  getContentByIdWithLanguageFallback,
-  getEntitiesByIdListWithLanguageFallback,
-} from '../helpers'
+
+import { UPGRADED_BASIC_NEMESIS_CARDS_DB_KEY } from './constants'
+
+import * as types from 'aer-types/types'
 
 ///////////
 // STATE //
@@ -30,23 +41,82 @@ export const initialState: State = {
 // ACTIONS //
 /////////////
 
-export type Action = Selected.Action
+export enum ActionTypes {
+  TOGGLE_ALL = 'Settings/Expansions/UpgradedBasicNemesisCards/TOGGLE_ALL',
+}
+
+export const mainActions = {
+  toggleAll: (expansionId: string, toggleType: types.ToggleType) =>
+    createAction(ActionTypes.TOGGLE_ALL, {
+      expansionId,
+      toggleType,
+    }),
+}
+
+export type MainAction = ActionsUnion<typeof mainActions>
 
 export const actions = {
   selected: Selected.actions,
   ids: Ids.actions,
   content: Content.actions,
+  main: mainActions,
 }
+
+export type Action = Selected.Action | MainAction
 
 /////////////
 // REDUCER //
 /////////////
 
-export const Reducer = combineReducers({
-  selected: Selected.Reducer,
-  ids: Ids.Reducer,
-  content: Content.Reducer,
-})
+export const Reducer = reduceReducers(
+  combineReducers<State>({
+    selected: Selected.Reducer,
+    ids: Ids.Reducer,
+    content: Content.Reducer,
+  }),
+  (state: State, action: Action | ExpansionAction) => {
+    switch (action.type) {
+      case ExpansionActionTypes.TOGGLE_ALL_EXPANSION_CONTENT:
+      case ActionTypes.TOGGLE_ALL: {
+        const { expansionId, toggleType } = action.payload
+
+        const allUpgradedBasicNemesisCardsOfExpansion = state.ids.filter(
+          (id) => state.content.ENG[id].expansion === expansionId
+        )
+
+        const newSelected =
+          toggleType === 'deselect'
+            ? []
+            : allUpgradedBasicNemesisCardsOfExpansion
+
+        const newSelectedUpgradedBasicNemesisCards =
+          newSelected.length === 0
+            ? state.selected.filter(
+                (id) => state.content.ENG[id].expansion !== expansionId
+              )
+            : [...new Set([...state.selected, ...newSelected])]
+
+        const newState = {
+          ...state,
+          selected: newSelectedUpgradedBasicNemesisCards,
+        }
+
+        return loop(
+          newState,
+          Cmd.run(setToDb, {
+            args: [UPGRADED_BASIC_NEMESIS_CARDS_DB_KEY, newState],
+            successActionCreator: actions.selected.setToDBSuccessful,
+            failActionCreator: actions.selected.setToDBFailed,
+          })
+        )
+      }
+
+      default: {
+        return state
+      }
+    }
+  }
+)
 
 ///////////////
 // SELECTORS //
@@ -62,7 +132,7 @@ const getExpansionId = (_: unknown, props: { expansionId: string }) =>
 const getUpgradedBasicNemesisCardIdsByExpansionId = createSelector(
   [Content.selectors.getContent, Ids.selectors.getIds, getExpansionId],
   (content, ids, expansionId) =>
-    ids.filter(id => content.ENG[id].expansion === expansionId)
+    ids.filter((id) => content.ENG[id].expansion === expansionId)
 )
 
 const getUpgradedBasicNemesisCardsByExpansionId = createSelector(
@@ -81,7 +151,7 @@ const getUpgradedBasicNemesisCardList = createSelector(
     LanguageSelectors.getLanguagesByExpansion,
   ],
   (content, ids, languages) =>
-    ids.map(id => getContentByIdWithLanguageFallback(languages, content, id))
+    ids.map((id) => getContentByIdWithLanguageFallback(languages, content, id))
 )
 
 const getSelectedUpgradedBasicNemesisCards = createSelector(
@@ -91,7 +161,33 @@ const getSelectedUpgradedBasicNemesisCards = createSelector(
     LanguageSelectors.getLanguagesByExpansion,
   ],
   (content, ids, languages) =>
-    ids.map(id => getContentByIdWithLanguageFallback(languages, content, id))
+    ids.map((id) => getContentByIdWithLanguageFallback(languages, content, id))
+)
+
+const getSelectedUpgradedBasicNemesisCardsByExpansionId = createSelector(
+  [
+    getSelectedUpgradedBasicNemesisCards,
+    getUpgradedBasicNemesisCardsByExpansionId,
+  ],
+  (
+    selectedUpgradedBasicNemesisCardIds,
+    expansionUpgradedBasicNemesisCardIds
+  ) => {
+    return selectedUpgradedBasicNemesisCardIds.filter((id) =>
+      expansionUpgradedBasicNemesisCardIds.includes(id)
+    )
+  }
+)
+
+const getAllUpgradedBasicNemesisCardsOfExpansionSelected = createSelector(
+  [
+    getSelectedUpgradedBasicNemesisCardsByExpansionId,
+    getUpgradedBasicNemesisCardsByExpansionId,
+  ],
+  (selectedUpgradedBasicNemesisCardIds, allUpgradedBasicNemesisCardIds) =>
+    allUpgradedBasicNemesisCardIds.every((id) =>
+      selectedUpgradedBasicNemesisCardIds.includes(id)
+    )
 )
 
 export const selectors = {
@@ -101,4 +197,6 @@ export const selectors = {
   getUpgradedBasicNemesisCardsByExpansionId,
   getUpgradedBasicNemesisCardList,
   getSelectedUpgradedBasicNemesisCards,
+  getSelectedUpgradedBasicNemesisCardsByExpansionId,
+  getAllUpgradedBasicNemesisCardsOfExpansionSelected,
 }
