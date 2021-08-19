dd4f5350205ebba967a36be508d857a63549a53e
M.Weseloh <milanthehawk@gmx.de>
2021-08-19 12:09:05 +0200

    [FEATURE] german translation for wave2
    Issue: none
    
    The following german translations were added
    - complete wave 2 (War Eternal, The Outer Dark, The Void)
    - 3 german promo packages (Drown in Flames, Nemeses cards, Fleeting Vision)

--- src/aer-data/src/DE/index.ts
+++ src/aer-data/src/DE/index.ts
@@ -1,13 +1,21 @@
 import { IExpansionData } from '../../../aer-types/types'
 
 import { aeonsEndData } from './aeonsEnd'
+import { outerDarkData } from './outerDark'
+import { promosData } from './promos'
 import { theDepthsData } from './theDepths'
 import { theNamelessData } from './theNameless'
+import { theVoidData } from './theVoid'
+import { warEternalData } from './warEternal'
 
 const DE: IExpansionData = {
   AE: aeonsEndData,
   Depths: theDepthsData,
   Nameless: theNamelessData,
+  OD: outerDarkData,
+  TV: theVoidData,
+  WE: warEternalData,
+  promos: promosData,
 }
 
 export default DE
