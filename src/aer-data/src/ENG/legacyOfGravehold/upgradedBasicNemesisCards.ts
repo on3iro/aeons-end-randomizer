74ec260597251d85ff5ce388d5c6b7340afde4bb
Theo Salzmann <theo.salzmann@sandstorm.de>
2022-11-05 21:53:18 +0100

    FIX: Stifle nemesis card tier

--- src/aer-data/src/ENG/legacyOfGravehold/upgradedBasicNemesisCards.ts
+++ src/aer-data/src/ENG/legacyOfGravehold/upgradedBasicNemesisCards.ts
@@ -132,7 +132,7 @@ export const upgradedBasicNemesisCards: UpgradedBasicNemesisCard[] = [
     id: 'Stifle',
     name: 'Stifle',
     expansion: 'LOG',
-    tier: 3,
+    tier: 2,
     type: 'Attack',
     effect: `
       <p>
