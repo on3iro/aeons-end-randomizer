73623a2a255049dbd3c5a8ed7012e10ecd3a5928
Maxence WILMES <maxencewilmes@gmail.com>
2022-12-21 22:10:26 +0100

    [QOL] Added language selection tooltip
    I added a small expansion panel with a little text to guide users as where to find the language selection.
    Did not add any tests, feel free to add it if needed.

--- src/components/pages/Settings/index.tsx
+++ src/components/pages/Settings/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Expansions from './Expansions'
 import Supply from './Supply'
+import Language from './Language'
 
 const Settings = () => (
   <React.Fragment>
     <Expansions />
     <Supply />
+    <Language />
   </React.Fragment>
 )
 
