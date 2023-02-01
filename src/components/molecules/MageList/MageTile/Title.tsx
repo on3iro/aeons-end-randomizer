62e50b95a817b44d524bcdc28559968423b67b9f
Dan Rosart <Torgen@users.noreply.github.com>
2023-02-01 01:42:55 -0800

    [FEATURE] Add mage titles to barracks cards (#478)
    * [FEATURE] Add mage titles to barracks cards
    
    If you sort the mages by name, the main way to distinguish mages with multiple versions is by their title. Previously this was only visible on the Show Details page, which required clicking in and out for each mage.
    
    * Ensure space for mage title, even when it's empty
    
    This incidentally reduces the margin above the show details button.

---
+++ src/components/molecules/MageList/MageTile/Title.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro'
+import Typography from '@material-ui/core/Typography'
+
+const Title = styled(Typography)`
+  color: #fff;
+  height: 1em;
+`
+
+export default Title
