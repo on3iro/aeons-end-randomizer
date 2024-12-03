54d6896e32c943f89edb52c741b4be77204ec006
deusabianca <73909818+deusabianca@users.noreply.github.com>
2024-12-03 03:03:01 -0500

    Update nemeses.ts

--- src/aer-data/src/ENG/talesOfOldGravehold/nemeses.ts
+++ src/aer-data/src/ENG/talesOfOldGravehold/nemeses.ts
@@ -1,13 +1,3 @@
 import { Nemesis } from '../../../../aer-types/types'
 
-export const nemeses: Nemesis[] = [
-  {
-    expansion: 'TC',
-    name: 'Absorbing Wraith',
-    id: 'AbsorbingWraith',
-    health: 85,
-    difficulty: 8,
-    expeditionRating: 4,
-    additionalInfo: '',
-  },
-]
+export const nemeses: Nemesis[] = []
